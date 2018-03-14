49660e37735d924bc898f531749ddfdb5365b7cb
Ana Asins <aasins@tsiberia.es>
2018-03-14 17:00:42 +0100

    inicio

---
+++ src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+import { AjustesPage, Ajustes2Page, ModalPage, Pagina2Page, Pagina3Page, PrincipalPage, TabsPage } from "../pages/index.paginas";
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    AjustesPage,
+    Ajustes2Page,
+    ModalPage,
+    Pagina2Page,
+    Pagina3Page,
+    PrincipalPage,
+    TabsPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    AjustesPage,
+    Ajustes2Page,
+    ModalPage,
+    Pagina2Page,
+    Pagina3Page,
+    PrincipalPage,
+    TabsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
